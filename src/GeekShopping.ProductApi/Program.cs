c83d4626ee4f0d1b2e15eb80e17be1972b275aba
Willian Viegas <willianwed98@gmail.com>
2022-11-15 20:34:55 -0300

    Initial Commit

---
+++ src/GeekShopping.ProductApi/Program.cs
@@ -0,0 +1,34 @@
+using GeekShopping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+IConfiguration configuration = builder.Configuration;
+var connection = configuration["MySQlConnection:MySQlConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+UseMySql(connection,
+new MySqlServerVersion(
+    new Version(8, 0, 31))));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
